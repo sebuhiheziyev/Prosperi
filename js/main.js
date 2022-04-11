e15311f8bb7183a45cb9578ce315ec4e9bb0e49b
Sabuxhi <sebuhiheziyev39@mail.com>
2022-04-12 02:46:47 +0400

    Slider work

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 "use strict";
-$(window).on('load', function(){
+$(window).on('load', function () {
 
     // open and close menu
-    $(".menuOpen").click(function(){
+    $(".menuOpen").click(function () {
         //icons
         $(".menuOpen").addClass("d-none");
         $(".menuClose").removeClass("d-none");
 
+
         //menu div
         $(".menu").removeClass("d-none");
         $("#nav").addClass("navbar-open");
-       
+
     });
 
-    $(".menuClose").click(function(){
+    $(".menuClose").click(function () {
         //icons
         $(".menuClose").addClass("d-none");
         $(".menuOpen").removeClass("d-none");
@@ -21,14 +22,66 @@ $(window).on('load', function(){
         //menu div
         $(".menu").addClass("d-none");
         $("#nav").removeClass("navbar-open");
-
-
     });
 
 
 
-    
-   
+
+    // open quiz
+    $(".go-quiz-btn").click(function () {
+        $(".go-quiz").hide();
+
+        //open quiz
+        $(".quiz-area").removeClass("d-none");
+        $(".quiz-area").addClass("d-block");
+
+    });
+
+    //back quiz
+    $(".back").click(function () {
+        console.log("salam");
+    });
+
+
+    //change quiz 
+    var activSliderCount = 1;
+    var mySliders = $(".mySlider");
+
+
+
+    $(".quiz-answer").click(function () {
+        var quiz =   $(this).parents(".mySlider");
+        quiz.addClass("d-none");
+        
+        var quizType = quiz.attr("dg");
+
+
+
+        if(quizType == 0){
+           $(".layoutFirst").css('width', '100%');
+        }
+        else{
+            $(".layoutSecond").css('width', '70%');
+           
+        }
+
+        // $(this).parents(".mySlider").addClass("d-none");
+        activSliderCount += 1;
+        changeSlider(activSliderCount);
+    });
+
+    function changeSlider (n){
+        var i = 0;
+        for(var mySlider of mySliders){
+           i++;
+           if(i == n){
+                mySlider.classList.remove(
+                    'd-none',
+                );
+           }
+        }
+    }
+
 });
 
 
